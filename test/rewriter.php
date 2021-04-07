19553239c919adbe8dba7898388cf92d9411c796
Sagleft <sagleft@inbox.ru>
2021-04-07 18:56:06 +0300

    add test article

---
+++ test/rewriter.php
@@ -0,0 +1,11 @@
+<?php
+    require_once __DIR__ . "/../src/Rewriter.php";
+
+    $article = "cientists have been leaving government posts since President Donald Trump took office in 2017, and those positions, by and large, have not been replaced. This shortage has led to insufficient regulation enforcement, limited data distribution, and a paucity of researchers, according to an analysis of public employee records by The Washington Post.";
+
+    $rewriter = new TextTools\Rewriter();
+    $article2 = $rewriter->Do($article);
+
+    echo "before: " . PHP_EOL . $article . PHP_EOL;
+    echo PHP_EOL;
+    echo "after: " . PHP_EOL . $article2 . PHP_EOL;
